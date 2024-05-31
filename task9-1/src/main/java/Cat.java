d3f1570175e7038d380816a45861cd5d14e9f0d5
Kirill <88333835+Kirirun4@users.noreply.github.com>
2024-05-31 13:09:11 +0400

    Update Cat.java

--- task9-1/src/main/java/Cat.java
+++ task9-1/src/main/java/Cat.java
@@ -12,7 +12,7 @@ public class Cat {
             @JsonProperty("text") String text,
             @JsonProperty("type") String type,
             @JsonProperty("user") String user,
-            @JsonProperty("upvotes") String upvotes)
+            @JsonProperty("upvotes") Integer upvotes)
     {
         this.id = id;
         this.text = text;
